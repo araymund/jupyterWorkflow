4430abb1293a38d063d6903a974b86c36292984a
Alfredo Raymundo <araymund@imp.mx>
2018-03-10 12:39:31 -0600

    Move data download to jupyterworkflow package

---
+++ jupyterworkflow/data.py
@@ -0,0 +1,31 @@
+import os
+import pandas as pd
+import seaborn as sns
+from urllib.request import urlretrieve
+
+FREMONT_URL = 'https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessType=DOWNLOAD'
+def get_fremont_data(filename = 'Fremont.csv', url = FREMONT_URL,
+		     force_download=False):
+    """Download and cahe the fremot data
+
+   Parameters
+   ----------
+   filename : string (optional)
+	location to save the data
+   url : string (optional)
+	web location of the data
+   force_download : bool (optional)
+        if true, force redownload of data
+
+   Returns
+   -------
+   data : pandas.DataFrame
+	The fremont data
+   """
+
+    if force_download or not os.path.exists(filename):
+        urlretrieve(url, filename)
+    data = pd.read_csv('Fremont.csv', index_col = 'Date', parse_dates = True)
+    data.columns = ['West', 'East']
+    data['Total'] = data['West'] + data['East']
+    return data
